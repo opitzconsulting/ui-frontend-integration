577d88b2ab269caebd442d299d2c5be8a5893b20
RAU <stephan.rauh@opitz-consulting.com>
2019-05-21 16:42:02 +0200

    Revert "updated to a current Angular 7"
    This reverts commit 1ed17d10f8aa405231d71f7e960b4d9eebffaf72.

--- ng-micro-frontends-1/scripts/build-all/index.js
+++ ng-micro-frontends-1/scripts/build-all/index.js
@@ -1,6 +1,6 @@
-const path = require('path');
-const fs = require('fs');
-const childProcess = require('child_process');
+const path = require("path");
+const fs = require("fs");
+const childProcess = require("child_process");
 
 const basePath =
   path
@@ -12,14 +12,17 @@ const basePath =
 const modules = fs.readdirSync(basePath);
 
 for (let module of modules) {
-  if (module !== 'common' && module !== 'scripts' && module === 'product') {
+  if (module !== "common" && module !== "scripts") {
     const stats = fs.lstatSync(basePath + module);
     if (stats.isDirectory()) {
-      const sourcePath = basePath + module + path.sep + 'source' + path.sep + module + path.sep;
-      if (fs.existsSync(sourcePath) && fs.existsSync(sourcePath + 'package.json')) {
+      const sourcePath =
+        basePath + module + path.sep + "source" + path.sep + module + path.sep;
+      if (
+        fs.existsSync(sourcePath) &&
+        fs.existsSync(sourcePath + "package.json")
+      ) {
         console.log(`npm run build in ${module}/source/${module}:`);
         childProcess.execSync(`cd ${sourcePath} && npm run build`);
-        // console.log(`cd ${sourcePath} && npm run build`);
       }
     }
   }
